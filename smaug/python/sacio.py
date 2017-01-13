1c91117c6b6178b426c50a11fbab656e4096547c
Michael <mike@flight-000>
2017-01-13 15:05:52 +0000

    file readwrite functions added to sacio module example in slice3dto2d

--- smaug/python/sacio.py
+++ smaug/python/sacio.py
@@ -3,13 +3,13 @@ import numpy as np
 #filename='../../../configs/hydro/3D_128_spic_asc.ini'
 
 #called as
+#from sacio import *
 #alldat,modelinfo=read_sac_ascii('../../../configs/hydro/3D_128_spic_asc.ini')
 
 
 def read_sac_ascii(filename):
     file = open(filename,'rb')
-
-                                                                  
+                                                                
     #read 5 sac file header lines
     
     #1 opozmf_mhd22    #name line 
@@ -90,3 +90,64 @@ def read_sac_ascii(filename):
 
 
 
+#filename='../../../configs/sac_test_asc.ini'
+
+#called as
+#alldat,modelinfo=read_sac_ascii('../../../configs/sac_test_asc.ini')
+
+
+
+def write_sac_ascii(filename, alldat, modelinfo):
+    
+    file = open(filename,'wb')
+    
+    
+    
+    
+    #this script assumes data has been read using a routine such as sac-read3-ascii.py
+    #the following variables are assumed
+    #nits
+    #time
+    #ndim
+    #nvar
+    #nfields
+    
+    #dim[2] or dim[3]
+    
+    #gamma
+    #eta
+    #grav1
+    #grav2
+    #grav3
+    
+    #all data is contained in an array alldat of shape nfields+ndim,dim[0],dim[1]
+    
+    
+    #write header lines
+    
+    #header='sac_test_asc'
+    header=modelinfo[0]
+    
+    
+    
+    head1=str(nits)+" "+str(time)+" "+str(ndim)+" "+str(nvar)+" "+str(nfields)
+    
+    if ndim==2:
+        head2=str(dim[0])+" "+str(dim[1])
+    elif ndim==3:
+        head2=str(dim[0])+" "+str(dim[1])+" "+str(dim[2])    
+    
+    #warning may need to explicityly write the adiabatic parameter and correct gravitational parameters here
+    head3="1.66667E+00  0.00000E+00  1.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00"
+    
+    if ndim==2:
+        head4="x y h m1 m2 e b1 b2 eb rhob bg1 bg2   gamma eta   grav1 grav2"
+    elif ndim==3:
+        head4="x y z h m1 m2 m3 e b1 b2 b3 eb rhob bg1 bg2 bg3   gamma eta   grav1 grav2 grav3"
+        
+    for i1 in range(dim[0]*dim[1]*dim[2]):
+        for i2 in range(dim[0]*dim[1]*dim[2]):
+            for i3 in range(dim[0]*dim[1]*dim[2]):
+                line=""
+                for j in range(ndim+nfields):
+                    line=line+str(alldat[i1,i2,i3,j])
--- smaug/python/slice3dto2d.py
+++ smaug/python/slice3dto2d.py
@@ -6,6 +6,16 @@ Created on Fri Jan 13 09:24:09 2017
 @author: mike
 """
 
+import numpy as np
+from sacio import read_sac_ascii
+from sacio import write_sac_ascii
+
+alldat,modelinfo=read_sac_ascii('../../../configs/hydro/3D_128_spic_asc.ini')
+
+#modelinfo=(header,nits, time, ndim, nvar, nfields,dim,head3,head4)
+dim=modelinfo[6]
+ndim=modelinfo[3]
+nfields=modelinfo[5]
 
 pos1=63
 pos2=63
@@ -14,4 +24,8 @@ pos3=63
 alldatslice=alldat[:,pos2,:,:]
 alldatslice=np.reshape(alldatslice,(dim[0],dim[2],ndim+nfields),order='F')
 
-newalldatslice=np.delete(alldatslice,(1),axis=2)
+newalldatslice=np.delete(alldatslice,(1),axis=2)  #delete a column of data the y positions
+
+print(newalldatslice[:,63,0])  #show height
+print(newalldatslice[:,63,10]) # energy
+print(newalldatslice[:,63,11]) #
