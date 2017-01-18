c63894533db2a6096e680c9213911ba8974533a9
Michael <mike@flight-000>
2017-01-18 21:08:40 +0000

    Read 3D config and write 2D slice to sac format ascii file

--- smaug/python/sacio.py
+++ smaug/python/sacio.py
@@ -218,3 +218,84 @@ def read_sac_bin(filename):
     modelinfo=(header,nits, time, ndim, nvar, nfields,dim,head3,head4)
 
     return alldat,modelinfo
+
+
+def write_sac_bin(filename, alldat, modelinfo):
+    
+    file = open(filename,'wb')    
+    
+    #this script assumes data has been read using a routine such as sac-read3-ascii.py
+    #the following variables are assumed
+    #nits
+    #time
+    #ndim
+    #nvar
+    #nfields
+    
+    #dim[2] or dim[3]
+    
+    #gamma
+    #eta
+    #grav1
+    #grav2
+    #grav3
+    
+    #all data is contained in an array alldat of shape nfields+ndim,dim[0],dim[1]
+    
+    
+    #write header lines
+    
+    #header='sac_test_asc'
+    header=modelinfo[0]
+    #modelinfo=(header,nits, time, ndim, nvar, nfields,dim,head3,head4)
+    #dim=[128,128]
+    #ndim=2
+    #nfields=12
+    dim=modelinfo[6]
+    ndim=modelinfo[3]
+    nfields=modelinfo[5]
+    time=modelinfo[2]
+    nits=modelinfo[1]
+    nvar=modelinfo[4]
+    
+    
+    head1=str(nits)+" "+str(time)+" "+str(ndim)+" "+str(nvar)+" "+str(nfields)+"\n"
+    
+    if ndim==2:
+        head2=str(dim[0])+" "+str(dim[1])+"\n"
+    elif ndim==3:
+        head2=str(dim[0])+" "+str(dim[1])+" "+str(dim[2])+"\n"    
+    
+    #warning may need to explicityly write the adiabatic parameter and correct gravitational parameters here
+    head3="1.66667E+00  0.00000E+00  1.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00"+"\n"
+    
+    if ndim==2:
+        head4=b"x y h m1 m2 e b1 b2 eb rhob bg1 bg2   gamma eta   grav1 grav2"+"\n"
+    elif ndim==3:
+        head4=b"x y z h m1 m2 m3 e b1 b2 b3 eb rhob bg1 bg2 bg3   gamma eta   grav1 grav2 grav3"+"\n"
+    
+    file.write(header.encode('utf-8'))
+    file.write(head1.encode('utf-8'))     
+    file.write(head2.encode('utf-8'))
+    file.write(head3.encode('utf-8'))
+    file.write(head4.encode('utf-8'))     
+        
+    if ndim==3:    
+        for i3 in range(dim[2]):
+            for i2 in range(dim[1]):
+                for i1 in range(dim[0]):
+                    line=""
+                    for j in range(ndim+nfields):
+                        line=line+str(alldat[i1,i2,i3,j])
+                    line=line+"\n"
+                    file.write(line.encode('utf-8'))
+                    
+    if ndim==2:    
+        for i2 in range(dim[1]):
+            for i1 in range(dim[0]):
+                line=""
+                for j in range(ndim+nfields):              
+                    line=line+" "+str(alldat[i1,i2,j])
+                line=line+"\n"
+                file.write(line.encode('utf-8'))    
+                
--- smaug/python/slice3dto2d.py
+++ smaug/python/slice3dto2d.py
@@ -29,3 +29,14 @@ newalldatslice=np.delete(alldatslice,(1),axis=2)  #delete a column of data the y
 print(newalldatslice[:,63,0])  #show height
 print(newalldatslice[:,63,10]) # energy
 print(newalldatslice[:,63,11]) #
+
+newdim=[128,128]
+
+nhead3="1.66667E+00  0.00000E+00  1.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00"
+nhead4="x y h m1 m2 e b1 b2 eb rhob bg1 bg2   gamma eta   grav1 grav2"
+
+newmodelinfo=(modelinfo[0],modelinfo[1], modelinfo[2], 2, modelinfo[4], 10,newdim,nhead3,nhead4)
+
+write_sac_ascii('../../../configs/hydro/2D_128_spic_asc.ini', newalldatslice, newmodelinfo)
+
+
