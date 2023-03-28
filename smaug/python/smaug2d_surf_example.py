3a38b557abccd2f4db14306f165dace38ab63910
mikeg2105 <mikeg2105@gmail.com>
2023-03-28 16:39:37 +0100

    work ozt test on bede care with periodic BCs, hyperdiffusion parameters and time step these set manually

---
+++ smaug/python/smaug2d_surf_example.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[75]:
+
+
+import numpy as np
+from numpy import *
+import scipy.io
+from scipy import special
+import matplotlib
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import struct
+
+#get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[154]:
+
+
+file = open('../../../out/zeroOT_5000.out','rb')
+
+
+
+file.seek(0,2)
+eof = file.tell()
+file.seek(0,0)
+
+name = file.read(79)
+nit = fromfile(file,dtype=int32,count=1)
+t = fromfile(file,dtype=float64,count=1)
+ndim=fromfile(file,dtype=int32,count=1)
+neqpar=fromfile(file,dtype=int32,count=1)
+nw=fromfile(file,dtype=int32,count=1)
+#ndata = fromfile(file,dtype=int32,count=ndim)[:ndim]
+ndata = fromfile(file,dtype=int32,count=2)
+varbuf = fromfile(file,dtype=float,count=6)[:6]
+varnames = file.read(79)
+
+#typedef enum vars {rho, mom1, mom2, energy, b1, b2,energyb,rhob,b1b,b2b} CEV;
+datcount=(nw+ndim)*ndata[0]*ndata[1]
+#alldat=fromfile(file,dtype=float,count=datcount)[:(nw+ndim)*ndata[0]*ndata[1]]
+alldat=fromfile(file,dtype=float,count=786432)
+if size(alldat)<(nw+ndim)*ndata[0]*ndata[1]:
+    alldat=resize(alldat,(nw+ndim)*ndata[0]*ndata[1])
+#alldat=np.reshape(alldat,(nw+ndim,ndata[0],ndata[1],),'C')
+alldat=np.reshape(alldat,(12,256,256),'C')
+
+file.close()
+
+
+# In[155]:
+
+
+print(ndata[0])
+
+
+# In[156]:
+
+
+print ('varnames:',varnames)
+print ('nit:',nit)
+print ('name:',name)
+print ('t:',t)
+print ('ndim:',ndim)
+print ('neqpar:',neqpar)
+print ('nw:',nw)
+print ('varbuf:',varbuf)
+
+
+# In[157]:
+
+
+dens=alldat[2,:,:]
+print(np.min(dens))
+print(np.max(dens))
+
+
+# In[158]:
+
+
+im2 = plt.imshow(dens, cmap=plt.cm.viridis, alpha=.9, interpolation='bilinear',vmin=0.07,vmax=0.352)
+
+plt.show()
+
+
+# In[29]:
+
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+X = alldat[0,:,:]
+Y = alldat[1,:,:]
+
+
+dens = alldat[2,:,:]
+#bsq=alldat[6,:,:]*alldat[6,:,:]+alldat[7,:,:]*alldat[7,:,:]
+#bmag=sqrt(bsq)
+
+surf = ax.plot_surface(X, Y, dens, rstride=1, cstride=1, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+#ax.set_zlim(-1.01, 1.01)
+
+#ax.zaxis.set_major_locator(LinearLocator(10))
+#ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+ax.view_init(elev=90,azim=0) 
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
+
+
+# In[12]:
+
+
+fig = plt.figure(figsize=plt.figaspect(1.))
+ax = fig.add_subplot(2, 1, 1,projection='3d')
+
+#X = alldat[0,:,:]
+#Y = alldat[1,:,:]
+
+
+energy = alldat[5,:,:]
+bsq=alldat[6,:,:]*alldat[6,:,:]+alldat[7,:,:]*alldat[7,:,:]
+bmag=sqrt(bsq)
+
+surf = ax.plot_surface(X, Y, energy, rstride=1, cstride=1, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+#ax.set_zlim(-1.01, 1.01)
+
+#ax.zaxis.set_major_locator(LinearLocator(10))
+#ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+ax.view_init(elev=90,azim=0) 
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+
+
+ax = fig.add_subplot(2, 1, 2,projection='3d')
+surf = ax.plot_surface(X, Y, bmag, rstride=1, cstride=1, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+#ax.set_zlim(-1.01, 1.01)
+
+#ax.zaxis.set_major_locator(LinearLocator(10))
+#ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+ax.view_init(elev=90,azim=0) 
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
+
+
+# In[ ]:
+
+
+
+
--- smaug/src/boundary.cu
+++ smaug/src/boundary.cu
@@ -12,6 +12,7 @@
 // kernel function (CUDA device)
 /////////////////////////////////////
 #include "../include/gradops_b.cuh"
+
 extern "C"
 __global__ void boundary_parallel(struct params *p, struct bparams *bp, struct state *s,  real *wmod, int order, int dir, int field)
 {
@@ -24,73 +25,129 @@ __global__ void boundary_parallel(struct params *p, struct bparams *bp, struct s
   int ni=p->n[0];
   int nj=p->n[1];
 
-   int ip,jp,ipg,jpg;
+  int ip,jp,ipg,jpg;
   int iia[NDIM];
   int dimp=((p->n[0]))*((p->n[1]));
- #ifdef USE_SAC_3D
-   int kp,kpg;
-   real dz=p->dx[2];
-   dimp=((p->n[0]))*((p->n[1]))*((p->n[2]));
-#endif
-   //int ip,jp,ipg,jpg;
 
+  #ifdef USE_SAC_3D
+   kp=iindex/(nj*ni);
+   jp=(iindex-(kp*(nj*ni)))/ni;
+   ip=iindex-(kp*nj*ni)-(jp*ni);
+#else
+    jp=iindex/ni;
+   ip=iindex-(jp*ni);
+#endif
 
 
 
 int shift=order*NVAR*dimp;
 
-  #ifdef USE_SAC_3D
-   iia[2]=k=iindex/(nj*ni);
-   iia[1]=j=(iindex-(k*(nj*ni)))/ni;
-   iia[0]=i=iindex-(k*nj*ni)-(j*ni);
-
-#else
-    iia[1]=j=iindex/ni;
-   iia[0]=i=iindex-(j*ni);
-#endif
 
 
-     #ifdef USE_SAC_3D
-           for( f=rho; f<=b3; f++)
-     #else
-           for( f=rho; f<=b2; f++)
-     #endif
-             {
-         #ifdef USE_SAC_3D
-      if(i<((p->n[0])) && j<((p->n[1]))  && k<((p->n[2])))
-     #else
-       if(i<((p->n[0])) && j<((p->n[1])))
-     #endif
-	{
+     iia[0]=ip;
+     iia[1]=jp;
+     k=0;
 
-
-
-               //bc3_periodic1_b(wmod+order*NVAR*dimp,p,iia,f);  //for OZT
-                bc3_periodic1_dir_b(wmod+order*NVAR*dimp,p,iia,f,dir);
-     #ifdef USE_SAC_3D
-         ;//if((f!=mom1 || f !=mom2 || f != mom3) && (p->it)>0)
-      #else
-         ;//if(f!=mom1 || f !=mom2 )
-      #endif
-                // bc3_cont_cd4_b(wmod+order*NVAR*dimp,p,iia,f);  //for BW
-
-
-                //  bc3_fixed_b(wmod+order*NVAR*dimp,p,iia,f,0.0);
-
-                //  bc3_fixed_dir_b(wmod+order*NVAR*dimp,p,bp,iia,f,dir);
-
-	}
-
-               }
+      //for( f=rho; f<=b2; f++)
+      //{
+      f=field;
+	       if(i<((p->n[0])) && j<((p->n[1])))
+		{
+		       bc3_periodic1_dir_b(wmod+order*NVAR*dimp,p,iia,f,dir);  //for OZT
+		       //  bc3_cont_cd4_b(wmod+order*NVAR*dimp,p,iia,f);  //for BW
+		       //  bc3_fixed_b(wmod+order*NVAR*dimp,p,iia,f,0.0);
+		}
+       //}
 
  __syncthreads();
 
 
+
+
+
+
 }
 
+extern "C"
+__global__ void boundary0_parallel(struct params *p, struct bparams *bp, struct state *s,  real *wmod, int order, int dir, int field)
+{
+
+  int iindex = blockIdx.x * blockDim.x + threadIdx.x;
+  int i,j;
+  int index,k;
+  int f;
+  int iplowerb, ipupperb;
+  int ni=p->n[0];
+  int nj=p->n[1];
+
+  int ip,jp,ipg,jpg,kp;
+  //int iia[NDIM];
+  int dimp=((p->n[0]))*((p->n[1]));
+
+  #ifdef USE_SAC_3D
+   kp=iindex/(nj*ni);
+   jp=(iindex-(kp*(nj*ni)))/ni;
+   ip=iindex-(kp*nj*ni)-(jp*ni);
+#else
+    jp=iindex/ni;
+   ip=iindex-(jp*ni);
+#endif
+
+kp=0;
+
+int shift=order*NVAR*dimp;
+
+
+
+    // iia[0]=ip;
+    // iia[1]=jp;
+     k=0;
+
+      //for( f=rho; f<=b2; f++)
+      //{
+      //f=field;
+	      // if(i<((p->n[0])) && j<((p->n[1])))
+		//{
+		      // bc3_periodic1_dir1_b(wmod+order*NVAR*dimp,p,iia,f,dir);  //for OZT
+		       //  bc3_cont_cd4_b(wmod+order*NVAR*dimp,p,iia,f);  //for BW
+		       //  bc3_fixed_b(wmod+order*NVAR*dimp,p,iia,f,0.0);
+		//}
+       //}
+
+
+
+
+
+kp=0;
+f=field;
+
+iplowerb=(ip==0 || ip==1);
+ipupperb=((ip==((p->n[0])-1)) || (ip==((p->n[0])-2)));
+
+wmod[shift+encode3_b(p,ip,jp,kp,f)]=(iplowerb?wmod[encode3_b(p,(p->n[0])-4+ip,jp,kp,f)+shift]:wmod[shift+encode3_b(p,ip,jp,kp,f)]);
+wmod[shift+encode3_b(p,ip,jp,kp,f)]=(ipupperb?wmod[encode3_b(p,4-(p->n[0])+ip,jp,kp,f)+shift]:wmod[shift+encode3_b(p,ip,jp,kp,f)]);
+
+
+
+             /*  if((ip==0 || ip==1) )
+                    wmod[encode3_b(p,ip,jp,kp,f)+shift]=wmod[encode3_b(p,(p->n[0])-4+ip,jp,kp,f)+shift];
+
+                else if(((ip==((p->n[0])-1)) || (ip==((p->n[0])-2))) )
+                    wmod[encode3_b(p,ip,jp,kp,f)+shift]=wmod[encode3_b(p,4-(p->n[0])+ip,jp,kp,f)+shift];*/
+
+
+
+
+ __syncthreads();
+
+
+
+
+
+}
 
 extern "C"
-__global__ void setboundary_parallel(struct params *p, struct bparams *bp, struct state *s,  real *wmod, int order, int dir, int field)
+__global__ void boundary1_parallel(struct params *p, struct bparams *bp, struct state *s,  real *wmod, int order, int dir, int field)
 {
 
   int iindex = blockIdx.x * blockDim.x + threadIdx.x;
@@ -101,165 +158,95 @@ __global__ void setboundary_parallel(struct params *p, struct bparams *bp, struc
   int ni=p->n[0];
   int nj=p->n[1];
 
-                real val=0;
-  int nk;
+  int jplowerb, jpupperb;
+
+  int ip,jp,kp,ipg,jpg;
   int iia[NDIM];
   int dimp=((p->n[0]))*((p->n[1]));
- #ifdef USE_SAC_3D
-   dimp=((p->n[0]))*((p->n[1]))*((p->n[2]));
-   nk=p->n[2];
-#endif
-
-
-   /* real *wtest;
-   int i1,i2,i3;
-
-    switch(dir)
-    {
-     case 0:
-	i1=127;
-	i2=63;
-	i3=63;
-     break;
-     case 1:
-	i1=63;
-	i2=127;
-	i3=63;
-     break;
-
-     case 2:
-	i1=63;
-	i2=63;
-	i3=127;
-     break;
-
-
-    }*/
 
   #ifdef USE_SAC_3D
-   iia[2]=k=iindex/(nj*ni);
-   iia[1]=j=(iindex-(k*(nj*ni)))/ni;
-   iia[0]=i=iindex-(k*nj*ni)-(j*ni);
-
+   kp=iindex/(nj*ni);
+   jp=(iindex-(kp*(nj*ni)))/ni;
+   ip=iindex-(kp*nj*ni)-(jp*ni);
 #else
-    iia[1]=j=iindex/ni;
-   iia[0]=i=iindex-(j*ni);
+    jp=iindex/ni;
+   ip=iindex-(jp*ni);
 #endif
+
+
 
 int shift=order*NVAR*dimp;
 
 
-         #ifdef USE_SAC_3D
-      if(i<((p->n[0])) && j<((p->n[1]))  && k<((p->n[2])))
-     #else
-       if(i<((p->n[0])) && j<((p->n[1])))
-     #endif
-	{
+
+   //  iia[0]=ip;
+  //   iia[1]=jp;
+   //  k=0;
+
+      //for( f=rho; f<=b2; f++)
+      //{
+
+	      // if(i<((p->n[0])) && j<((p->n[1])))
+		//{
+		       //bc3_periodic1_dir2_b(wmod+order*NVAR*dimp,p,iia,f,dir);  //for OZT
+		       //  bc3_cont_cd4_b(wmod+order*NVAR*dimp,p,iia,f);  //for BW
+		       //  bc3_fixed_b(wmod+order*NVAR*dimp,p,iia,f,0.0);
+		//}
+       //}
 
 
 
-              // bc3_periodic1_b(wmod+order*NVAR*dimp,p,iia,f);  //for OZT
+f=field;
+kp=0;
 
-     #ifdef USE_SAC_3D
-         ;//if((f!=mom1 || f !=mom2 || f != mom3) && (p->it)>0)
-      #else
-         ;//if(f!=mom1 || f !=mom2 )
-      #endif
+jplowerb=(jp==0 || jp==1);
+jpupperb=((jp==((p->n[1])-1)) || (jp==((p->n[1])-2)));
 
-		  bc3_setfixed_dir_b(wmod,p,bp,iia,field,dir);
+wmod[shift+encode3_b(p,ip,jp,kp,f)]=(jplowerb?wmod[shift+encode3_b(p,ip,(p->n[1])-4+jp,kp,f)]:wmod[shift+encode3_b(p,ip,jp,kp,f)]);
+wmod[shift+encode3_b(p,ip,jp,kp,f)]=(jpupperb?wmod[shift+encode3_b(p,ip,4-(p->n[1])+jp,kp,f)]:wmod[shift+encode3_b(p,ip,jp,kp,f)]);
 
-                  /*if(i==i1 && j==i2 && k==i3)
-                  {
-                    wtest=wmod;//+order*NVAR*dimp;
-                    p->test=wmod[encode3_b(p,i,j,k,field)];
 
-                   }*/
-	}
+            /*    if((jp==0 || jp==1)  )
+                  wmod[shift+encode3_b(p,ip,jp,kp,f)]=wmod[shift+encode3_b(p,ip,(p->n[1])-4+jp,kp,f)];
+
+                else if(((jp==((p->n[1])-1)) || (jp==((p->n[1])-2))) )
+                  wmod[shift+encode3_b(p,ip,jp,kp,f)]=wmod[shift+encode3_b(p,ip,4-(p->n[1])+jp,kp,f)];*/
+
+
+
 
  __syncthreads();
 
 
+
+
+
+
 }
 
 
 
 
-//extern "C"
+
 int cuboundary(struct params **p, struct bparams **bp,struct params **d_p, struct bparams **d_bp, struct state **d_s,  real **d_wmod,  int order,int idir,int field)
 {
-
+//printf("bound\n");
 
  dim3 dimBlock(dimblock, 1);
 
 int numBlocks = ((dimproduct_b(*p)+numThreadsPerBlock-1)) / numThreadsPerBlock;
-
-
-
-
-int i1,i2,i3;
-
-
-
-if(((*p)->it)==-1)
+if(idir==0 && (*p)->boundtype[field][0][0]==0)
 {
-    //cudaMemcpy(*d_p,*p, sizeof(struct params), cudaMemcpyHostToDevice);
-    setboundary_parallel<<<numBlocks, numThreadsPerBlock>>>(*d_p,*d_bp,*d_s, *d_wmod, order,idir,field);
-
-    //cudaMemcpy(*bp,*d_bp, sizeof(struct bparams), cudaMemcpyDeviceToHost);
-    //cudaMemcpy(*p,*d_p, sizeof(struct params), cudaMemcpyDeviceToHost);
-
-     //
-    /*switch(idir)
-    {
-     case 0:
-	i1=127;
-	i2=63;
-	i3=63;
-
-      printf("dir=%d field=%d value=%20.10g  fixed=%20.10g\n",idir,field,(*p)->test,(*bp)->fixed1[encodefixed13_b(*p,1+(*p)->n[0]-i1,i2,i3,field)]);
-     break;
-     case 1:
-
-	i1=63;
-	i2=127;
-	i3=63;
-
-      printf("dir=%d field=%d value=%20.10g  fixed=%20.10g\n",idir,field,(*p)->test,(*bp)->fixed2[encodefixed23_b(*p,i1,1+(*p)->n[1]-i2,i3,field)]);
-     break;
-     case 2:
-
-	i1=63;
-	i2=63;
-	i3=127;
-
-      printf("dir=%d field=%d value=%20.10g  fixed=%20.10g\n",idir,field,(*p)->test,(*bp)->fixed3[encodefixed33_b(*p,i1,i2,1+(*p)->n[2]-i3,field)]);
-     break;
-
-    }*/
-
-
+//printf("bound 0\n");
+      // boundary_parallel<<<numBlocks, numThreadsPerBlock>>>(*d_p,*d_bp,*d_s, *d_wmod, order,0,field);
+	boundary0_parallel<<<numBlocks, numThreadsPerBlock>>>(*d_p,*d_bp,*d_s, *d_wmod, order,0,field);
 }
-else
+    cudaThreadSynchronize();
+if(idir==1 && (*p)->boundtype[field][1][0]==0)
 {
-    boundary_parallel<<<numBlocks, numThreadsPerBlock>>>(*d_p,*d_bp,*d_s, *d_wmod, order,idir,field);
-
-    cudaThreadSynchronize();
-   /* boundary_parallel<<<numBlocks, numThreadsPerBlock>>>(*d_p,*d_bp,*d_s, *d_wmod, order,1,field);
-    cudaThreadSynchronize();
-#ifdef USE_SAC_3D
-    boundary_parallel<<<numBlocks, numThreadsPerBlock>>>(*d_p,*d_bp,*d_s, *d_wmod, order,2,field);
-    cudaThreadSynchronize();
-#endif  */
+//printf("bound 1\n");
+    //boundary_parallel<<<numBlocks, numThreadsPerBlock>>>(*d_p,*d_bp,*d_s, *d_wmod, order,1,field);
+        boundary1_parallel<<<numBlocks, numThreadsPerBlock>>>(*d_p,*d_bp,*d_s, *d_wmod, order,1,field);
 }
-
-
-
+    cudaThreadSynchronize();
 }
-
-
-
-
-
-
-
-
